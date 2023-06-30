5f68313737d3a60d47c4cb74fc0fcd7acad95347
Akhil Sharma <akhilsharma6721@gmail.com>
2023-06-30 10:08:35 +0530

    first commit

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,12 @@
+'use client'
+import {BiSolidUserCircle} from 'react-icons/bi';
+
+const Avatar = () => {
+  return (    
+        <BiSolidUserCircle 
+            className='text-gray-400 h-[24px] w-[24px]'
+        />
+    )
+}
+
+export default Avatar
