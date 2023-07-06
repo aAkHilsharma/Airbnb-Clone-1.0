b54179e3f6510f1f2952b0665ced42a7051b7dbe
Akhil Sharma <akhilsharma6721@gmail.com>
2023-07-06 22:30:54 +0530

    created card list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+  
+
+  if(listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
   return (
-    <div>
-      Hello Airbnb!
-    </div>
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6  gap-8">
+            {listings.map((listing: any) => {
+              return (
+                <ListingCard 
+                  key={listing.id}
+                  data={listing}
+                />
+              )
+            })}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
