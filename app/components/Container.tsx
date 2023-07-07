69aad91bae8441b2c68827e7bd8320d17bdb374e
Akhil Sharma <akhilsharma6721@gmail.com>
2023-07-07 14:48:24 +0530

    fixed responsiveness issue

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -7,12 +7,13 @@ interface ContainerProps {
 const Container: React.FC<ContainerProps> = ({children}) => {
     return (<div
         className="
+        px-5
         max-w-[2520px]
         mx-auto
         xl:px-20
         md:px-10
-        sm:px-2
-        xs:px-4
+        sm:px-5
+        xs:px-5
         "
     >
         {children}
